8ea63cc2ae95c4783d8a72b9ec83bc67e4eed519
Michele Memoli <michele.memoli@gmail.com>
2025-03-26 10:35:36 +0000

    feat(app): example of using shared-db

--- libs/module-projects-core/src/lib/infra/definitions/project-repo.ts
+++ libs/module-projects-core/src/lib/infra/definitions/project-repo.ts
@@ -1,16 +1,26 @@
 import { Fail, Ok } from "rich-domain";
 import { ProjectRepoTrait, Project } from "../../domain/project/index.js";
+import { Db } from "@err/shared-db";
 
-export const createProjectRepo = (): ProjectRepoTrait => {
+export interface CreateProjectRepoDeps {
+  db: Db;
+}
+
+export const createProjectRepo = (
+  deps: CreateProjectRepoDeps
+): ProjectRepoTrait => {
   return {
     fetchForId: async (id) => {
+      const query = await deps.db.query.usersTable.findFirst();
+      console.log(query);
+
       const buildResult = Project.builder()
         .withName("My First Project")
         .build();
       if (buildResult.isFail())
         return Fail(`Failed to build Project: ${buildResult.error()}`);
       const project = buildResult.value();
-      return await Promise.resolve(Ok(project, { id: id.value() }));
+      return Ok(project, { id: id.value() });
     },
     save: async (project) => {
       return await Promise.resolve(Ok(project));
--- libs/module-projects-core/src/lib/infra/impl/project-repo.ts
+++ libs/module-projects-core/src/lib/infra/impl/project-repo.ts
@@ -1,3 +1,6 @@
+import { db } from "@err/shared-db";
 import { createProjectRepo } from "../definitions/project-repo.js";
 
-export const projectRepo = createProjectRepo();
+export const projectRepo = createProjectRepo({
+  db,
+});
